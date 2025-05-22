6a304de6f8d4e7418095bb189a14a18d92237248
Josué García <gar24918@uvg.edu.gt>
2025-05-21 19:42:19 -0600

    Se agregaron métodos faltantes principalmente para los filtros, se agrego buscar actores y obtener todas las peliculas en donde actua un actor en especifico

--- controllers/actor_controller.py
+++ controllers/actor_controller.py
@@ -13,3 +13,24 @@ class ActorController:
     def get_all_actors():
         query = "MATCH (a:Actor) RETURN a"
         return Neo4jConnection().query(query)
+    
+    @staticmethod
+    def search_actors(keyword):
+        query = """
+        MATCH (a:Actor)
+        WHERE toLower(a.name) CONTAINS toLower($keyword)
+        RETURN a
+        LIMIT 20
+        """
+        return Neo4jConnection().query(query, {"keyword": keyword})
+    
+    @staticmethod
+    def get_movies_by_actor(actor_name, min_weight=0.5):
+        query = """
+        MATCH (a:Actor {name: $actor_name})<-[r:HAS_ACTOR]-(m:Movie)
+        WHERE r.peso >= $min_weight
+        RETURN m {.id, .title, .year, actor_weight: r.peso} AS movie
+        ORDER BY r.peso DESC
+        LIMIT 20
+        """
+        return Neo4jConnection().query(query, {"actor_name": actor_name, "min_weight": min_weight})
