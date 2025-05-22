538fd7f5e24eed376d764cbc661bc56f55c8bfed
Josué García <gar24918@uvg.edu.gt>
2025-05-21 20:06:40 -0600

    Se agrego el método para ver todas las peliculas dirigidas por un director en especifico

--- controllers/director_controller.py
+++ controllers/director_controller.py
@@ -13,3 +13,14 @@ class DirectorController:
     def get_all_directors():
         query = "MATCH (d:Director) RETURN d"
         return Neo4jConnection().query(query)
+
+    @staticmethod
+    def get_movies_by_director(director_name, min_weight=0.5):
+        query = """
+        MATCH (d:Director {name: $director_name})<-[r:DIRECTED_BY]-(m:Movie)
+        WHERE r.peso >= $min_weight
+        RETURN m {.id, .title, .year, director_weight: r.peso} AS movie
+        ORDER BY r.peso DESC
+        LIMIT 20
+        """
+        return Neo4jConnection().query(query, {"director_name": director_name, "min_weight": min_weight})
