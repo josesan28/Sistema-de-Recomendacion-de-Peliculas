d98e3bc110b8718d221120b52604b4b6b9087021
Josué García <gar24918@uvg.edu.gt>
2025-05-21 20:20:27 -0600

    Se agrego el método para obtener las pelicuas de algun genero en especifico

--- controllers/genre_controller.py
+++ controllers/genre_controller.py
@@ -13,3 +13,14 @@ class GenreController:
     def get_all_genres():
         query = "MATCH (g:Genre) RETURN g"
         return Neo4jConnection().query(query)
+    
+    @staticmethod
+    def get_movies_by_genre(genre_name, min_weight=0.5):
+        query = """
+        MATCH (g:Genre {name: $genre_name})<-[r:HAS_GENRE]-(m:Movie)
+        WHERE r.peso >= $min_weight
+        RETURN m {.id, .title, .year, genre_weight: r.peso} AS movie
+        ORDER BY r.peso DESC
+        LIMIT 20
+        """
+        return Neo4jConnection().query(query, {"genre_name": genre_name, "min_weight": min_weight})
